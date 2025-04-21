04cec636e00248c66f97f974ca5e43289148bec3
Kanan Farzali <kanan.farzali@gmail.com>
2025-04-21 09:40:09 +0200

    adjust font size for a subtitle

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
       <section className="card-cta">
         <div className="flex flex-col gap-6 max-w-lg">
           <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
-          <p className="text-lg">
+          <p className="text-base sm:text-lg">
             Practice on real interview questions & get instant feedback
           </p>
 
