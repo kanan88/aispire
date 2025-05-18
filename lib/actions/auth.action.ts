e5f4eacc51e918c5fe5c23d5d005ff838e716b28
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-05-18 14:52:06 +0200

    implement server action to fetch interviews

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -110,3 +110,18 @@ export const isAuthenticated = async () => {
 
   return !!user
 }
+
+export const getInterviewByUserId = async (
+  userId: string
+): Promise<Interview[] | null> => {
+  const interviews = await db
+    .collection('interviews')
+    .where('userId', '==', userId)
+    .orderBy('createdAt', 'desc')
+    .get()
+
+  return interviews.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  })) as Interview[]
+}
